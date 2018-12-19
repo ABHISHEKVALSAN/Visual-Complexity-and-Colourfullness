19c3a9f276bd2e022fa104eb7b3547e21891a3d0
ABHISHEKVALSAN <abhishekvalsan9999@gmail.com>
2018-12-19 20:45:06 +0530

    Half Indian Gov.

---
+++ getScreenshots.py
@@ -0,0 +1,41 @@
+import subprocess
+import sys
+import numpy as np
+import webcolors
+import re
+import string
+import sys
+import unidecode
+from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
+from itertools import groupby
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.support.ui import WebDriverWait
+def saveScreenshot(driver,url):
+	imgName=url.split('//')[1].split('/')[0]
+	driver.save_screenshot('/home/abhiavk/git/Visual-Complexity-and-Colourfullness/Training/IndiaGov/'+imgName+'.png')
+def main(filename):
+	options 	= Options()
+	options.add_argument("--headless")
+	driver		= webdriver.Chrome(chrome_options=options)
+	driver.set_window_size(1024,768)
+	urls		=open(filename,"r")
+	for url in urls:
+		url=url[:-1]
+		print(url)
+		driver.get(url)
+		WebDriverWait(driver, timeout=10).until(lambda x: x.find_elements_by_tag_name('body'))
+		saveScreenshot(driver,url)
+	driver.quit()
+if __name__=="__main__":
+	filename=sys.argv[-1]
+	main(filename)
---
+++ trainCNN.py
@@ -0,0 +1,11 @@
+
+model = Sequential()
+model.add(Conv2D(20, (5, 5), padding="same", input_shape=(20, 20, 1), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(50, (5, 5), padding="same", activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(500, activation="relu"))
+model.add(Dense(32, activation="softmax"))
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=32, epochs=10, verbose=1)
